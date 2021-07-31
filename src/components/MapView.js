ad79990ef0e33ec1082a3a5009abd810e884ddec
Darryl Bayliss <darryl@darrylbayliss.net>
2021-07-31 21:45:30 +0100

    Add google maps component

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,60 +1,45 @@
-import React, { Component, Fragment } from 'react'
-import { Map, Marker } from 'react-mapkit'
-import NavBar from '../components/NavBar'
+import React, { Component } from 'react'
+import { Marker, Map, GoogleApiWrapper } from 'google-maps-react'
 
-class MapView extends Component {
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+}
+
+export class MapView extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      search: '',
       electricPoints: []
     }
-
-    this.onSearchButtonTapped = this.onSearchButtonTapped.bind(this)
-  }
-
-  async getElectricPoints () {
-    return await fetch('http://localhost:3001/electricPoints')
-  }
-
-  async getElectricPointsForTown (town) {
-    return await fetch(`http://localhost:3001/electricPoints?town=${town}`)
-  }
-
-  onSearchButtonTapped (searchText) {
-    this.setState({ electricPoints: [] })
-    this.getElectricPointsForTown(searchText)
-      .then(response => response.json())
-      .then(latestElectricPoints => this.setState({ electricPoints: latestElectricPoints.ChargeDevice }))
-      .catch(error => console.log(error))
   }
 
   render () {
-    return (
-      <Fragment>
-        <NavBar onSearchButtonTapped={this.onSearchButtonTapped}/>
-        <Map tokenOrCallback="eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRNQk43VDlaMlgifQ.eyJpYXQiOjE2MjA0ODk0NTcsImV4cCI6MTY1MjAyNTQ1NywiaXNzIjoiM0NZTEZUVDNCSCJ9.wPCZsJLYa8H_vHsTzJ_j00O-rZUOOj2aIRwepHOOGfiv3pcLmjyYkZYnZwVy5NGSkangdjWJftgjc0h29hh7kw"
-             region= {{
-               latitude: 53.483959,
-               longitude: -2.244644,
-               latitudeSpan: 13.0,
-               longitudeSpan: 13.0
-             }}
-        >
+    const electricPoints = this.state.electricPoints
 
-        {
-        this.state.electricPoints.map(electricPoint => (
-          <Marker key={electricPoint.ChargeDeviceId}
-          latitude={Number(electricPoint.ChargeDeviceLocation.Latitude)}
-          longitude={Number(electricPoint.ChargeDeviceLocation.Longitude)}
-          title={electricPoint.ChargeDeviceName} />
-        ))
+    return (
+      <Map>
+        google={this.props.google}
+        zoom={5}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: 53.4105800,
+            lng: -2.9779400
+          }
         }
-        </Map>
-      </Fragment>
+        { electricPoints.map(electricPoint =>
+         <Marker key={electricPoint.ChargeDeviceId}
+         name={electricPoint.ChargeDeviceName}
+         position={{ lat: electricPoint.ChargeDeviceLocation.Latitude, lng: electricPoint.ChargeDeviceLocation.Longitude }}/>
+        )
+         }
+      </Map>
     )
   }
 }
 
-export default MapView
+export default GoogleApiWrapper({
+  apiKey: ('')
+})(MapView)
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import MapView from '../components/MapView.js'
+import NavBar from '../components/NavBar.js'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      search: '',
+      electricPoints: []
+    }
+
+    this.onSearchButtonTapped = this.onSearchButtonTapped.bind(this)
+  }
+
+  async getElectricPoints () {
+    return await fetch('http://localhost:3001/electricPoints')
+  }
+
+  async getElectricPointsForTown (town) {
+    return await fetch(`http://localhost:3001/electricPoints?town=${town}`)
+  }
+
+  onSearchButtonTapped (searchText) {
+    this.setState({ electricPoints: [] })
+    this.getElectricPointsForTown(searchText)
+      .then(response => response.json())
+      .then(latestElectricPoints => this.setState({ electricPoints: latestElectricPoints.ChargeDevice }))
+      .catch(error => console.log(error))
+  }
+
   render () {
     return (
-      <MapView />
+      <div>
+      <Fragment>
+      <NavBar onSearchButtonTapped={this.onSearchButtonTapped}/>
+      <MapView electricPoints={this.state.electricPoints}/>
+      </Fragment>
+      </div>
     )
   }
 }
